833adbefb46befaa8a31b45078b5695598bd1ad0
Fethisensei <fetam62@gmail.com>
2024-06-21 16:40:01 +0200

    feat: add new button in my game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,12 @@ import { SpecialAttackComponent } from './special-attack/special-attack.componen
 import { HealbtnComponent } from './healbtn/healbtn.component';
 import { PlayerHealthBarComponent } from './player-health-bar/player-health-bar.component';
 import { GiveUpbtnComponent } from './give-upbtn/give-upbtn.component';
-import { PictureComponent } from './picture/picture.component';
 import { OneshotComponent } from './oneshot/oneshot.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AttackbtnComponent, MonsterHealthBarComponent, SpecialAttackComponent, HealbtnComponent, PlayerHealthBarComponent, GiveUpbtnComponent, PictureComponent, OneshotComponent],
+  imports: [RouterOutlet, AttackbtnComponent, MonsterHealthBarComponent, SpecialAttackComponent, HealbtnComponent, PlayerHealthBarComponent, GiveUpbtnComponent, OneshotComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
